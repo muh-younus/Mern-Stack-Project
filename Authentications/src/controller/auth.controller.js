13c8cf3fe546bf5963e0baefcd0412caec737643
muh-younus <braq369@gmail.com>
2026-04-09 00:08:25 +0500

    added access and refresh token

--- Authentications/src/controller/auth.controller.js
+++ Authentications/src/controller/auth.controller.js
@@ -1,6 +1,9 @@
 import userModel from "../model/user.model.js";
 import crypto from "crypto"
 import jwt from 'jsonwebtoken'
+import config from '../config/config.js';
+import { decode } from "punycode";
+import sessionModel from "../model/session.model.js";
 
 export async function register(req,res){
 
@@ -14,7 +17,7 @@ export async function register(req,res){
         ]
     })
 
-    if(isAlreadyExist){
+    if(isAlreadyExist.length>0){
 
         return res.status(409).json({
             message: "username or email is aleady exist"
@@ -29,22 +32,124 @@ export async function register(req,res){
         password:hashPassword
     })
 
-    const token = jwt.sign({
-        id: user._id
+    const refreshToken = jwt.sign({
+        id: newUser._id
     },config.JWT_SECRET,
     {
-        expiresIn:"30d"
+        expiresIn:"7d"
+    })
+
+    const refTokenHash = crypto.createHash("sha256").update(refreshToken).digest("hex");
+
+    const session = await sessionModel.create({
+        user:newUser._id,
+        refreshTokenHash:refTokenHash,
+        ip: req.ip,
+        userAgent: req.headers["user-agent"]
+    })
+
+    const client = await sessionModel.findOne({user:session.user})
+    console.log("client",client)
+
+
+
+    const accessToken = jwt.sign({
+        id: newUser._id,
+        sessionId:session._id
+    },config.JWT_SECRET,
+    {
+        expiresIn:"1m"
+    })
+
+    
+
+    res.cookie("refreshToken",refreshToken,{
+
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 *60 * 60 * 1000
     })
 
     res.status(201).json({
         message: "user create successfully",
         user:{
-            username: user.username,
-            email: user.email
+            username: newUser.username,
+            email: newUser.email
         
         },
-        token
+        accessToken,
+        refreshToken
+
     })
 
 
+}
+
+export async function getMe(req,res){
+
+    const token = req.headers.authorization?.split(" ")[1]
+    console.log("token",token)
+    if(!token){
+
+        return res.status(401).json({
+            message: "token is not found"
+        })
+    }
+    const decoded = jwt.verify(token,config.JWT_SECRET)
+    console.log("decoded is",decoded)
+    
+    const user = await userModel.findById(decoded.id)
+    console.log("user is ",user)
+
+    if(!user){
+
+        return res.status(404).json({
+            message: "user not found"
+        })
+    }
+
+    return res.status(200).json({
+        message: "user fetched successfully",
+        username: user.username,
+        email: user.email
+    })
+    
+}
+
+export async function refreshToken(req,res){
+
+    const refreshToken = req.cookies.refreshToken;
+    if(!refreshToken){
+
+        return res.status(401).json({
+            message: "Refresh token not found"
+        })
+    }
+ 
+    const decoded = jwt.verify(refreshToken,config.JWT_SECRET)
+   const accessToken =jwt.sign({
+    id:decoded.id,
+   },config.JWT_SECRET,
+{
+    expiresIn: "14m"
+})
+
+const newRefreshToken = jwt.sign({
+    id: decoded.id
+},config.JWT_SECRET,
+{
+    expiresIn: "6d"
+})
+
+res.cookie("refreshToken",newRefreshToken,{
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 *60 * 60 * 60 * 1000
+})
+res.status(200).json({
+    message: "Access token refreshed successfully",
+    accessToken
+})
 }
--- Authentications/src/routes/auth.routes.js
+++ Authentications/src/routes/auth.routes.js
@@ -3,6 +3,20 @@ import * as authController from"../controller/auth.controller.js"
 
 const authRouter = Router()
 
+/*
+*POST:- api/auth/register
+*/
 authRouter.post("/register",authController.register)
+
+/*
+*GET:-api/auth/get-me
+*/
+authRouter.get("/get-me",authController.getMe)
+
+/* 
+*Get:-api/auth/refreshToken
+*/
+authRouter.get("/refresh-token",authController.refreshToken)
+
 
 export default authRouter
